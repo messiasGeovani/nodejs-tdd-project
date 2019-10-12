26898f3817a6f23e3c1a8203df2cc632d14f07f3
messiasGeovani <messias.geovani.lima@gmail.com>
2019-10-11 23:08:17 -0300

    environment database variables config

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,20 @@
 /**
+ * environment variables config
+ */
+require('dotenv').config({
+  path: process.env.NODE_ENV == 'test' ? '.env.test' : '.env'
+})
+
+/**
  * Database config object
  */
 module.exports = {
-  username: 'geovani',  
-  password: '1234',  
-  database: 'nodeauth',  
-  host: '127.0.0.1',  
-  dialect: 'postgres',
+  username: process.env.DB_USER,  
+  password: process.env.DB_PASS,  
+  database: process.env.DB_NAME,  
+  host: process.env.DB_HOST,  
+  dialect: process.env.DB_DIALECT || 'postgres',
+  storage: './__tests__/database.sqlite',
   operatorsAliases: false,
   logging: false,
   define: {
